58ea808cbf21f6b75d617023f2fc2342a16b92df
Pratham Raghuvanshi <141766108+Pratham22R@users.noreply.github.com>
2024-03-10 23:27:21 +0530

    Add files via upload

---
+++ pseudo code find s.py
@@ -0,0 +1,8 @@
+#S = initialize_specific_hypothesis()
+
+#for each positive example in training_set:
+    #for each attribute in positive example:
+        #if attribute in positive example is not consistent with S:
+            #generalize attribute in S
+
+#print("Final hypothesis:", S)
